841fb58831a60f4b33fbd071151f1067eb63c849
kymking <kimaniantidote655@gmail.com>
2025-12-05 12:49:19 +0300

    commit

--- lib/car.py
+++ lib/car.py
@@ -2,3 +2,11 @@ from vehicle import Vehicle
 
 class Car(Vehicle):
     pass
+    
+    class Car:
+     def __init__(self, speed, wheels):
+        self.speed = speed
+        self.wheels = wheels
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
